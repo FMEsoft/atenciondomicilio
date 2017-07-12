7ba0d84709e20bffe6588376324f4e8eea51bca0
PaoSantillan <pao.chxnge@gmail.com>
2017-07-12 15:16:14 -0300

    Agrega la conexion a la bd

--- mvc/controllers/atenciones.php
+++ mvc/controllers/atenciones.php
@@ -1,9 +1,39 @@
 <?php
 	require_once '../../vendor/autoload.php';
+	
+	include ('conexion.php');
 
 	$loader = new Twig_Loader_Filesystem('../views');
 
 	$twig = new Twig_Environment($loader, []);
+	
+	// Array de conexión con la base de datos
+	$config['db']='fme_mutual';
+	$config['dbuser']='root';
+	$config['dbpass']='';
+	$config['dbhost']='localhost';
+	$config['dbEngine']='MYSQL';
+	
+	$db = new CONEXION($config['dbhost'],$config['dbuser'],$config['dbpass'],$config['db']);
+	
+	//---------------CONSULTA DE PRUEBA----------------
+	$resultado = $db->select('SELECT * FROM fme_asistencia WHERE doctitu = 04191748');
+	
+	
+	if($resultado)
+	{
+            foreach($resultado as $res)
+                echo "IDNUM: " . $res['idnum'] . " " ;
+				echo "CODSER: " . $res['cod_ser'] . " ";
+				echo "NROORDATE: " . $res['nroordate'] . " ";
+				echo "DOCTITU: " . $res['doctitu'] . " ";
+				echo "NUMDOC: " . $res['numdoc'] . " ";
+				echo "NOMBRE: " . $res['nombre'] . " ";
+	}
+	else
+		echo "algo va mal";
+	
+	//---------------CONSULTA DE PRUEBA----------------
 
 	echo $twig->render('/Atenciones/nueva_atencion_1.html');	
 	
